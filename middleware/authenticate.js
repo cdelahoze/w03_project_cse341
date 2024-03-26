eb249874bc325955d95f23a75c34013401836bd7
cdelahoze <cdelahoze@hotmail.com>
2024-03-25 21:11:45 -0500

    new correction

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,8 +1,6 @@
 const isAuthenticated = (req, res, next) => {
   if (req.session.user === undefined) {
     return res.status(401).json("you do not have access.");
-  }else if (req.session.user !== undefined){
-    return res.status(200).json(`Logged in as ${req.session.user.displayName}`);
   }
   next();
 };
